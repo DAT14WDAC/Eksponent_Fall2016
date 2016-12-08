b37ff5dbe9e5aa105be9bc48d8487902b4ebae92
DAT14WDAC <tulipdana@gmail.com>
2016-12-08 12:44:36 +0100

    commit changes

--- Eksponent_Fall2016/Models/EmployeeSkills.cs
+++ Eksponent_Fall2016/Models/EmployeeSkills.cs
@@ -8,9 +8,13 @@ namespace Eksponent_Fall2016.Models
     public class EmployeeSkills
     {
         public int EmployeeSkillsId { get; set; }
+        public int Level { get; set; }
         public int SkillId { get; set; }
         public int EmployeeId { get; set; }
-        public int LevelId { get; set; }
+       
+
+        public virtual Skill Skill { get; set; }
+        public virtual Employee Employee { get; set; }
 
         public virtual ICollection<Skill> ISkills { get; set; }
         public virtual ICollection<Employee> IEmployees { get; set; }
